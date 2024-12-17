bf2a8e55a4b8f39099bb29dec942305513e6f9f5
pokarin1521 <h15m21yukimon@gmail.com>
2024-12-17 11:42:16 +0900

    気にしないで

--- Assets/Pokarin/Scripts/RifleBullet/RifleBullet.cs
+++ Assets/Pokarin/Scripts/RifleBullet/RifleBullet.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class RifleBullet : MonoBehaviour
 {
@@ -10,15 +11,25 @@ public class RifleBullet : MonoBehaviour
     [Header("速度")]
     [SerializeField] private float speed;
 
+    [Header("射撃から弾が消えるまでの時間")]
+    [SerializeField] private float destroyTime;
+
     /// <summary> 射撃対象 </summary>
     [NonSerialized] public Transform target;
 
-    private Vector3 direction;
+    private Vector3 position;
+
+    private float maxAcceleration = 100.0f;
+
+    private Vector3 velocity;
+
+    private float period = 10.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 3);
+        // 一定時間後に削除
+        Destroy(gameObject, destroyTime);
 
         // 対象がいる方向
         transform.LookAt(target.position);
@@ -27,6 +38,25 @@ public class RifleBullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        var acceleration = Vector3.zero;
+        var diff = target.position - position;
+
+        //速度velocityの物体がperiod秒後にdiff進むための加速度
+        acceleration += (diff - velocity * period) * 2f / (period * period);
+
+        if (acceleration.magnitude > maxAcceleration)
+        {
+            acceleration = acceleration.normalized * maxAcceleration;
+        }
+
+        period -= Time.deltaTime;
+
+        if (period < 0f)
+            return;
+
+        velocity += acceleration * Time.deltaTime;
+        position += velocity * Time.deltaTime;
+        transform.position = position;
+
     }
 }
