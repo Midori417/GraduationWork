b481e0113802a32fc463a57edc6dc125418c44d2
Midori417 <watanabemidori417@gmail.com>
2024-12-20 13:53:46 +0900

    エフェクトマテリアル変更

--- Assets/Midorin/Bullet/BulletCollision.cs
+++ Assets/Midorin/Bullet/BulletCollision.cs
@@ -7,16 +7,38 @@ public class BulletCollision : MonoBehaviour
     [SerializeField, Header("与えるダメージ")]
     private int damage;
 
+    [SerializeField, Header("衝突したときに自身を破壊するか")]
+    private bool isDead = false;
+
+    [SerializeField, Header("衝突時のエフェクト")]
+    private GameObject pfb_eff_hit;
+
     /// <summary>
     /// 衝突したときに処理
     /// </summary>
     /// <param name="other"></param>
     private void OnTriggerEnter(Collider other)
     {
+        // エフェクトを生成
+        if(pfb_eff_hit)
+        {
+            Instantiate(pfb_eff_hit, transform.position, transform.rotation);
+        }
+
+        // 機体に衝突したらダメージを与える
         if (other.gameObject.tag == "MS")
         {
             other.GetComponent<BaseMs>().Damage(damage);
+            if(isDead)
+            {
+                Destroy(gameObject);
+            }
         }
 
+        // 建物に衝突したら自身を破壊
+        if(other.gameObject.tag == "Ground" || other.gameObject.tag == "Building")
+        {
+            Destroy(gameObject);
+        }
     }
 }
